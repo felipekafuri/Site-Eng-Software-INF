fb6f55ff51a9bd34a13f968baac0b4e1a65626cc
Felipe Ramos Kafuri <felipekafuri@discente.ufg.br>
2020-03-17 12:49:48 -0300

    v.3

--- _cdn/js/home.js
+++ _cdn/js/home.js
@@ -7,3 +7,63 @@ btnScrollTop.addEventListener("click", function(e) {
     behavior: "smooth"
   });
 });
+
+const cur = document.getElementById("cur");
+
+cur.addEventListener("click", function(e) {
+  e.preventDefault();
+  const id = $(this).attr("href");
+  targetOffset = $(id).offset().top;
+
+  $("html, body").animate(
+    {
+      scrollTop: targetOffset
+    },
+    700
+  );
+});
+
+const hor = document.getElementById("hor");
+
+hor.addEventListener("click", function(e) {
+  e.preventDefault();
+  const id = $(this).attr("href");
+  targetOffset = $(id).offset().top;
+
+  $("html, body").animate(
+    {
+      scrollTop: targetOffset
+    },
+    700
+  );
+});
+
+const curs = document.getElementById("curs");
+
+curs.addEventListener("click", function(e) {
+  e.preventDefault();
+  const id = $(this).attr("href");
+  targetOffset = $(id).offset().top;
+
+  $("html, body").animate(
+    {
+      scrollTop: targetOffset
+    },
+    700
+  );
+});
+
+const newyt = document.getElementById("newyt");
+
+newyt.addEventListener("click", function(e) {
+  e.preventDefault();
+  const id = $(this).attr("href");
+  targetOffset = $(id).offset().top;
+
+  $("html, body").animate(
+    {
+      scrollTop: targetOffset
+    },
+    700
+  );
+});
