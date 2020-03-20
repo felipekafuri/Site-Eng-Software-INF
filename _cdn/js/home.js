c9c7811483a31bad90e35be53ca79df9e8177b2f
Felipe Ramos Kafuri <felipekafuri@discente.ufg.br>
2020-03-20 18:19:03 -0300

    Algumas animações

--- _cdn/js/home.js
+++ _cdn/js/home.js
@@ -67,3 +67,81 @@ newyt.addEventListener("click", function(e) {
     700
   );
 });
+
+//video animation
+$(document).ready(function() {
+  $(".video").hover(
+    //primeiro parametro
+    function() {
+      $(this).animate(
+        {
+          marginTop: "-=1%"
+        },
+        200
+      );
+    },
+    //segundo parametro
+    function() {
+      $(this).animate(
+        {
+          marginTop: "0%"
+        },
+        200
+      );
+    }
+  );
+});
+
+//video animation
+$(document).ready(function() {
+  $(".cartao").hover(
+    //primeiro parametro
+    function() {
+      $(this).animate(
+        {
+          marginTop: "-=1%"
+        },
+        200
+      );
+    },
+    //segundo parametro
+    function() {
+      $(this).animate(
+        {
+          marginTop: "0%"
+        },
+        200
+      );
+    }
+  );
+});
+
+//============ animçao fade in===========
+const faders = document.querySelectorAll(".fade-in");
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px"
+};
+const appearOnScroll = new IntersectionObserver(function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+
+const sliders = document.querySelectorAll(".slide-in");
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+});
